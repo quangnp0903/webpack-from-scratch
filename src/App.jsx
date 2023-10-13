4863020c6b7cf500cc9d5aad31a4221ef3147997
Quang Pham <quangnp0903@gmail.com>
2023-10-13 15:21:04 +0700

    config webpack

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <div className='app'>
-    <div className='card'>
-      <h1 className='title'>Our React is working :)</h1>
+  <div className="app">
+    <div className="card">
+      <h1 className="title">Our React is working :)</h1>
     </div>
   </div>
 );
